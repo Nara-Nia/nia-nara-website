f885f12da6e476b7234b2e0fbbef837bf0af033d
Diogo Caxiado <caxiadodev@gmail.com>
2025-04-21 14:48:04 -0300

    feat: Adicionando o chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "./components/ui/provider";
 import Routes from './router/routes';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes />
+      <Provider>
+        <Routes />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
